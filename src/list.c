335e4fdaf25da0862230ae77c5ac3803fe3beee4
dinamix <tristano.tenaglia@mcgill.ca>
2015-03-17 23:09:49 -0400

    File input running

--- src/list.c
+++ src/list.c
@@ -1,5 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "list.h"
+
+/*NODE *head;
+NODE *tail;
+NODE firstnode;
+head = &firstnode;
+
+void add(int nvalue) {
+	
+}*/
 
 LINKEDLIST createList(int nvalue) {
 	LINKEDLIST newlist;
@@ -10,3 +20,9 @@ LINKEDLIST createList(int nvalue) {
 	return newlist;
 }
 
+void addNode(LINKEDLIST* thislist,int newvalue) {
+	int newindex = thislist->tail->index + 1;
+	NODE newnode = {newindex,newvalue};	
+	thislist->tail->nextnode = &newnode;
+	thislist->tail = &newnode;
+}
--- src/main.c
+++ src/main.c
@@ -2,8 +2,60 @@
 #include <stdlib.h>
 #include "list.h"
 
+#define ARRAY_SIZE 100
+
+int filenotfound(FILE*);
+int filenull(char[]);
+
 int main() {
-	LINKEDLIST list = createList(20);
-	printf("%d\n",list.firstnode.nextnode);
+	//Create and initialize file
+	FILE *file;
+	char filename[ARRAY_SIZE];
+	do {
+		printf("Please enter filename : ");
+		scanf("%s",filename);
+		file = fopen(filename,"r");
+	}while(filenotfound(file));
+	
+	//Check if file has something in it
+	if(filenull(filename)) {
+		printf("Nothing found in file.\n");
+		return EXIT_SUCCESS;
+	}
+
+	//Initialize linkedlist with first int
+	int initial;
+	fscanf(file,"%d",&initial);	
+	LINKEDLIST list = createList(initial);
+
+	//Continue to take input till EOF
+	int nextint;
+	while(!feof(file)) {
+		fscanf(file,"%d",&nextint);
+		addNode(&list,nextint);
+	}
+	printf("%d\n",list.tail->value);
 	return EXIT_SUCCESS;
+}
+
+/**
+*This function ensures that the file exists
+**/
+int filenotfound(FILE *fp) {
+	if(fp == 0) return 1;
+	else return 0;
+}
+
+int filenull(char filename[ARRAY_SIZE]) {
+	FILE *testfile;
+	testfile = fopen(filename,"r");
+	getc(testfile);
+	if(feof(testfile)) {
+		fclose(testfile);
+		return 1;
+	}
+	else {
+		fclose(testfile);
+		return 0;
+	}
 }
